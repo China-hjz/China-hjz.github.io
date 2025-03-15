0ff11e1b296589cc6431662d0d8fffe6c895203e
HJZhao <Huang-Jingzhao@outlook.com>
2025-03-15 15:11:01 +0800

    Site updated: 2025-03-15 15:11:00

--- bing.php
+++ bing.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
 <?php
 $url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN";
 $curl = curl_init($url);
@@ -26,3 +29,5 @@ if ($imgurl) {
     exit('error');
 }
 ?>
+</body> 
+</html>
